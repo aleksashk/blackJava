fbfec391917b4322e2e1a0af5987853ebd64440b
Aleksandr <aleksandrphilimonov@gmail.com>
2022-06-30 16:04:51 +0300

    right practise

--- src/main/java/com/gmail/aleksandrphilimonov/generics/MainAppTest1.java
+++ src/main/java/com/gmail/aleksandrphilimonov/generics/MainAppTest1.java
@@ -5,14 +5,14 @@ import java.util.List;
 
 public class MainAppTest1 {
     public static void main(String[] args) {
-        List list = new ArrayList<>();
+        List<String> list = new ArrayList<>();
         list.add("OK");
-        list.add(5);
-        list.add(new StringBuilder("sb"));
-        list.add(new Cat());
+        list.add("" + 5);
+        list.add(new StringBuilder("sb").toString());
+        list.add(new Cat().toString());
 
-        for (Object o : list) {
-            System.out.println(o + " " + ((String)o).length());
+        for (String o : list) {
+            System.out.println(o + " " + o.length());
         }
     }
 }
