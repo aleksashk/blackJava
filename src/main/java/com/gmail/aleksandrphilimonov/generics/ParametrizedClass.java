db0f383954b07a73b729ac921e1ddd031d2f84ae
Aleksandr <aleksandrphilimonov@gmail.com>
2022-06-30 16:12:22 +0300

    generic Class

---
+++ src/main/java/com/gmail/aleksandrphilimonov/generics/ParametrizedClass.java
@@ -0,0 +1,33 @@
+package com.gmail.aleksandrphilimonov.generics;
+
+public class ParametrizedClass {
+    public static void main(String[] args) {
+        Info<String> infoString = new Info<>("privet");
+        System.out.println(infoString);
+        String valueS = infoString.getValue();
+        System.out.println(valueS);
+        Info<Integer> infoInteger = new Info<>(22);
+        System.out.println(infoInteger);
+        Integer valueI = infoInteger.getValue();
+        System.out.println(valueI);
+    }
+}
+
+class Info<T> {
+    private T value;
+
+    public Info(T value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "Info:\n\t{" +
+                "value = " + value +
+                '}';
+    }
+
+    public T getValue() {
+        return value;
+    }
+}
