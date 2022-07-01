21617fe2b087ab0a054e9786db4bc2b1bdb48143
Aleksandr <aleksandrphilimonov@gmail.com>
2022-07-01 21:51:38 +0300

    ArrayList methods example 1

---
+++ src/main/java/com/gmail/aleksandrphilimonov/collection/ArrayListEx1.java
@@ -0,0 +1,39 @@
+package com.gmail.aleksandrphilimonov.collection;
+
+import java.util.ArrayList;
+
+public class ArrayListEx1 {
+    public static void main(String[] args) {
+        ArrayList<String> arrayList1 = new ArrayList<>();
+        arrayList1.add("someString1");
+        arrayList1.add("someString2");
+        arrayList1.add("someString3");
+        arrayList1.add("someString4");
+        arrayList1.add(0, "someString5");
+        arrayList1.add(2, "someString6");
+
+        System.out.println("***********************************");
+        System.out.println(arrayList1.get(0));
+        System.out.println("***********************************");
+        arrayList1.set(0, "another string");
+        System.out.println(arrayList1.get(0));
+
+        System.out.println("***********************************");
+        System.out.println(arrayList1.remove(0));
+        System.out.println("***********************************");
+        System.out.println(arrayList1);
+
+        for (String s : arrayList1) {
+            System.out.println(s);
+        }
+        System.out.println("***********************************");
+        System.out.println(arrayList1.get(arrayList1.size() - 1));
+        System.out.println("***********************************");
+
+        for (int i = 0; i < arrayList1.size(); i++) {
+            System.out.println(arrayList1.get(i));
+        }
+
+    }
+}
+
---
+++ src/main/java/com/gmail/aleksandrphilimonov/collection/ArrayListMethods3.java
@@ -0,0 +1,35 @@
+package com.gmail.aleksandrphilimonov.collection;
+
+import java.util.ArrayList;
+
+public class ArrayListMethods3 {
+    public static void main(String[] args) {
+        ArrayList<String> arrayList1 = new ArrayList<>();
+        arrayList1.add("someString1");
+        arrayList1.add("someString2");
+        arrayList1.add("someString3");
+        arrayList1.add("someString3");
+        arrayList1.add("someString4");
+        System.out.println(arrayList1);
+
+//        ArrayList<String> arrayList2 = new ArrayList<>();
+//        arrayList2.add("!!!");
+//        arrayList2.add("???");
+//
+//        arrayList1.addAll(arrayList2);
+//        System.out.println(arrayList1);
+//        arrayList1.addAll(2, arrayList2);
+//        System.out.println(arrayList1);
+//        arrayList1.clear();
+//        System.out.println(arrayList1);
+
+        System.out.println(arrayList1.indexOf("someString3"));
+        System.out.println(arrayList1.lastIndexOf("someString3"));
+        System.out.println(arrayList1.size());
+//        System.out.println(arrayList1.isEmpty());
+//        arrayList1.clear();
+//        System.out.println(arrayList1.isEmpty());
+
+        System.out.println(arrayList1.contains("someString2"));
+    }
+}
