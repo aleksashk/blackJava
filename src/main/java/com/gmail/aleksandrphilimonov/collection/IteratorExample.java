2b9b455ba8b4772fc37703df77df348c71938a7b
Aleksandr <aleksandrphilimonov@gmail.com>
2022-07-01 22:19:33 +0300

    ArrayList Iterator

---
+++ src/main/java/com/gmail/aleksandrphilimonov/collection/IteratorExample.java
@@ -0,0 +1,28 @@
+package com.gmail.aleksandrphilimonov.collection;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class IteratorExample {
+    public static void main(String[] args) {
+        ArrayList<String> arrayList1 = new ArrayList<>();
+        arrayList1.add("someString1");
+        arrayList1.add("someString2");
+        arrayList1.add("someString3");
+        arrayList1.add("someString4");
+        arrayList1.add("someString77");
+
+        Iterator<String> iterator = arrayList1.iterator();
+//        while (iterator.hasNext()) {
+//            System.out.println(iterator.next());
+//        }
+
+//        while (iterator.hasNext()) {
+//            iterator.next();
+//            iterator.remove();
+//        }
+//        System.out.println(arrayList1);
+
+
+    }
+}
