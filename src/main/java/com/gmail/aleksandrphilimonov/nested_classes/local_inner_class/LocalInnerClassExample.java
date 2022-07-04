2a05d8a5819d6a2adcfe874c5cbeb0000ada77fe
Aleksandr <aleksandrphilimonov@gmail.com>
2022-07-04 19:36:12 +0300

    local inner class version 2

--- src/main/java/com/gmail/aleksandrphilimonov/nested_classes/local_inner_class/LocalInnerClassExample.java
+++ src/main/java/com/gmail/aleksandrphilimonov/nested_classes/local_inner_class/LocalInnerClassExample.java
@@ -3,32 +3,13 @@ package com.gmail.aleksandrphilimonov.nested_classes.local_inner_class;
 public class LocalInnerClassExample {
     public static void main(String[] args) {
         Math math = new Math();
-        math.getResult();
+        math.getResult(26, 5);
     }
 }
 
 class Math {
-    public void getResult() {
+    public void getResult(final int delimoe, final int delitel) {
         class Delenie {
-            private int delimoe;
-            private int delitel;
-
-            public int getDelimoe() {
-                return delimoe;
-            }
-
-            public void setDelimoe(int delimoe) {
-                this.delimoe = delimoe;
-            }
-
-            public int getDelitel() {
-                return delitel;
-            }
-
-            public void setDelitel(int delitel) {
-                this.delitel = delitel;
-            }
-
             public int getChasnoe() {
                 return delimoe / delitel;
             }
@@ -38,10 +19,8 @@ class Math {
             }
         }
         Delenie delenie = new Delenie();
-        delenie.setDelimoe(21);
-        delenie.setDelitel(4);
-        System.out.println("Delimoe = " + delenie.getDelimoe());
-        System.out.println("Delitel = " + delenie.getDelitel());
+        System.out.println("Delimoe = " + delimoe);
+        System.out.println("Delitel = " + delitel);
         System.out.println("Chasnoe = " + delenie.getChasnoe());
         System.out.println("Ostatok = " + delenie.getOstatok());
     }
