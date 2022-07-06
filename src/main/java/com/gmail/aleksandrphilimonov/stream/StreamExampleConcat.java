0ebadd74c9c205fe18cd68df294089de542e6227
Aleksandr <aleksandrphilimonov@gmail.com>
2022-07-06 23:06:24 +0300

    stream distinct

--- src/main/java/com/gmail/aleksandrphilimonov/stream/StreamExampleConcat.java
+++ src/main/java/com/gmail/aleksandrphilimonov/stream/StreamExampleConcat.java
@@ -7,8 +7,9 @@ public class StreamExampleConcat {
     public static void main(String[] args) {
         Stream<Integer> s1 = Stream.of(1, 3, 5, 7);
         Stream<Integer> s2 = Stream.of(-1, -2, 4, 0, -6, 7);
-        Stream<Integer> s3 = Stream.of(100,200,300,400);
-        Stream<Integer> s4 = Stream.concat(s1,s2);
-        System.out.println(s4);
+        Stream<Integer> s3 = Stream.of(100, 200, 100, 200, 100, 200, 300, 200, 300, 200, 200, 300, 200, 200, 300, 200, 300, 400);
+        Stream<Integer> s4 = Stream.concat(s1, s2);
+//        System.out.println(s4);
+        System.out.println(s3.distinct().collect(Collectors.toList()));
     }
 }
