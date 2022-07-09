9f15eb524f690c954d94dac8a74230946af652ab
Aleksandr <aleksandrphilimonov@gmail.com>
2022-07-09 21:20:18 +0300

    multiThreading volatile

---
+++ src/main/java/com/gmail/aleksandrphilimonov/multithreading/MulThExample10.java
@@ -0,0 +1,24 @@
+package com.gmail.aleksandrphilimonov.multithreading;
+
+public class MulThExample10 extends Thread {
+    volatile boolean b = true;
+
+    @Override
+    public void run() {
+        long counter = 0;
+        while (b) {
+            counter++;
+        }
+        System.out.println("loop is finished. counter = " + counter);
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        MulThExample10 thread = new MulThExample10();
+        thread.start();
+        Thread.sleep(3000);
+        System.out.println("Wake up!!");
+        thread.b = false;
+        thread.join();
+        System.out.println("end");
+    }
+}
