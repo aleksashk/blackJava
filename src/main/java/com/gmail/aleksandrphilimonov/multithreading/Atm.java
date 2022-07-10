f3a8c1d4763ecce54c2badddf240fe2950685121
Aleksandr <aleksandrphilimonov@gmail.com>
2022-07-10 18:21:24 +0300

    multiThreading interface Lock and class ReentrantLock method tryLock()

--- src/main/java/com/gmail/aleksandrphilimonov/multithreading/Atm.java
+++ src/main/java/com/gmail/aleksandrphilimonov/multithreading/Atm.java
@@ -4,12 +4,15 @@ import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class Atm {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Lock lock = new ReentrantLock();
 
         new Employee("Ivan", lock);
         new Employee("Helen", lock);
         new Employee("Olga", lock);
+
+        Thread.sleep(5000);
+
         new Employee("Richard", lock);
         new Employee("Steven", lock);
     }
@@ -27,16 +30,20 @@ class Employee extends Thread {
 
     @Override
     public void run() {
-        try {
-            System.out.println(name + " is waiting...");
-            lock.lock();
-            System.out.println(name + " is using ATM");
-            Thread.sleep(2000);
-            System.out.println(name + " finished");
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        } finally {
-            lock.unlock();
+        if (lock.tryLock()) {
+            try {
+//            System.out.println(name + " is waiting...");
+//            lock.lock();
+                System.out.println(name + " is using ATM");
+                Thread.sleep(2000);
+                System.out.println(name + " finished");
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            } finally {
+                lock.unlock();
+            }
+        } else {
+            System.out.println(name + " doesn't want waiting in the queue");
         }
     }
 }
