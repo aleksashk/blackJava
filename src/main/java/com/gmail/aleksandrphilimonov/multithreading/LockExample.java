db6b51d4ac8a1406d7a9f1e7ee4d41722c7bd573
Aleksandr <aleksandrphilimonov@gmail.com>
2022-07-10 18:11:26 +0300

    multiThreading interface Lock and class ReentrantLock

---
+++ src/main/java/com/gmail/aleksandrphilimonov/multithreading/LockExample.java
@@ -0,0 +1,78 @@
+package com.gmail.aleksandrphilimonov.multithreading;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class LockExample {
+    public static void main(String[] args) {
+        Call call = new Call();
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                call.mobileCall();
+            }
+        });
+
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                call.viberCall();
+            }
+        });
+
+        Thread thread3 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                call.watsAppCall();
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+        thread3.start();
+
+    }
+}
+
+class Call {
+    private Lock lock = new ReentrantLock();
+
+    void mobileCall() {
+        lock.lock();
+        try {
+            System.out.println("Mobile call starts");
+            Thread.sleep(2000);
+            System.out.println("Mobile call ends");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    void viberCall() {
+        lock.lock();
+        try {
+            System.out.println("Viber call starts");
+            Thread.sleep(4000);
+            System.out.println("Viber call ends");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    void watsAppCall() {
+        lock.lock();
+        try {
+            System.out.println("WatsApp call starts");
+            Thread.sleep(6000);
+            System.out.println("WatsApp call ends");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
+}
