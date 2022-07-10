12f36dcd0b4edb47f7b5f4201356427e244e4154
Aleksandr <aleksandrphilimonov@gmail.com>
2022-07-10 14:01:13 +0300

    multiThreading synchronized block example 4

---
+++ src/main/java/com/gmail/aleksandrphilimonov/multithreading/monitor_synchronized/SynchExample3.java
@@ -0,0 +1,66 @@
+package com.gmail.aleksandrphilimonov.multithreading.monitor_synchronized;
+
+public class SynchExample3 {
+    void mobileCall() {
+        System.out.println("mobile calls starts");
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("mobile call ends");
+    }
+
+    void viberCall() {
+        System.out.println("viber calls starts");
+        try {
+            Thread.sleep(6000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("viber call ends");
+    }
+
+    void watsAppCall() {
+        System.out.println("watsApp calls starts");
+        try {
+            Thread.sleep(9000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("watsApp call ends");
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread t1 = new Thread(new RunnableImplementationMobile());
+        t1.start();
+        Thread t2 = new Thread(new RunnableImplementationViber());
+        t2.start();
+        Thread t3 = new Thread(new RunnableImplementationWatsApp());
+        t3.start();
+    }
+}
+
+class RunnableImplementationMobile implements Runnable {
+
+    @Override
+    public void run() {
+        new SynchExample3().mobileCall();
+    }
+}
+
+class RunnableImplementationViber implements Runnable {
+
+    @Override
+    public void run() {
+        new SynchExample3().viberCall();
+    }
+}
+
+class RunnableImplementationWatsApp implements Runnable {
+
+    @Override
+    public void run() {
+        new SynchExample3().watsAppCall();
+    }
+}
